36763baa68cc7d9a913bb6a5c27778a897fa70a5
Joey Van Lierop <joey.vanlierop@me.com>
2020-05-06 03:00:45 -0600

    Add smooth scrolling.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import "normalize.css";
 import React from "react";
 import ReactDOM from "react-dom";
+import smoothscroll from "smoothscroll-polyfill";
 import App from "./App";
 import Root from "./components/Root";
 import "./styles/stylesheet.css";
 
+smoothscroll.polyfill();
 ReactDOM.render(
   <Root>
     <App />
