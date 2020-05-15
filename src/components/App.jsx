ab04c8319c41a9888f22ab779b8526dae799bc7f
Joey Van Lierop <joey.vanlierop@pureweb.com>
2020-05-15 12:23:07 -0600

    Update App to use new React Router link

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 import Home from "./Home";
 import Navbar from "./Navbar";
+import Background from "./Background";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      <Navbar id={"top-navbar"}>
-        <Navbar.Link href="#">Home</Navbar.Link>
-        <Navbar.Link href="#">Blog</Navbar.Link>
-        <Navbar.Link href="#">Notes</Navbar.Link>
-      </Navbar>
-      <Switch>
-        <Route path="/" exact component={Home} />
-      </Switch>
+      <Background>
+        <Navbar id={"top-navbar"}>
+          <Navbar.Link to="/">Home</Navbar.Link>
+          <Navbar.Link to="/blog">Blog</Navbar.Link>
+          <Navbar.Link to="/notes">Notes</Navbar.Link>
+        </Navbar>
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </Background>
     </Router>
   );
 }
