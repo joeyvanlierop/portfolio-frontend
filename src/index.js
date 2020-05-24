b3539eecd0b0b5f64ebcb8a2c34465ad2e7661df
Joey Van Lierop <joey.vanlierop@me.com>
2020-05-24 17:38:20 -0600

    Bootstrap react-snap into app

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import "normalize.css";
 import React from "react";
-import ReactDOM from "react-dom";
+import { hydrate, render } from "react-dom";
 import smoothscroll from "smoothscroll-polyfill";
 import App from "./components/App";
 import AppTheme from "./components/AppTheme";
 import "./styles/stylesheet.css";
 
 smoothscroll.polyfill();
-ReactDOM.render(
-  <AppTheme>
-    <App />
-  </AppTheme>,
-  document.getElementById("root")
-);
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(
+    <AppTheme>
+      <App />
+    </AppTheme>,
+    rootElement
+  );
+} else {
+  render(
+    <AppTheme>
+      <App />
+    </AppTheme>,
+    rootElement
+  );
+}
