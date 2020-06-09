a085cf0c3947cbda285ceff35ec836a39355f380
Joey Van Lierop <joey.vanlierop@me.com>
2020-06-08 21:07:32 -0600

    Use css for global styles

--- src/components/GlobalStyle.jsx
+++ src/components/GlobalStyle.jsx
@@ -1,27 +1,30 @@
-import { Global } from "@emotion/core";
+import { Global, css } from "@emotion/core";
 
 export default () => (
   <Global
-    styles={(theme) => ({
-      html: {
-        "line-height": "1.5",
-        "scroll-behavior": "smooth",
-        "box-sizing": "border-box",
-      },
+    styles={css`
+      html {
+        line-height: 1.5;
+        scroll-behavior: smooth;
+        box-sizing: border-box;
+      }
 
-      "html, body": {
-        height: "100%",
-      },
+      html,
+      body {
+        height: 100%;
+      }
 
-      "#root": {
-        display: "flex",
-        "flex-direction": "column",
-        "min-height": "100%",
-      },
+      #root {
+        display: flex;
+        flex-direction: column;
+        min-height: 100%;
+      }
 
-      "*, *:before, *:after": {
-        "box-sizing": "inherit",
-      },
-    })}
+      *,
+      *:before,
+      *:after {
+        box-sizing: inherit;
+      }
+    `}
   />
 );
