dbdf4605cc3aef286b2bcabed078cedfd560d817
Joey Van Lierop <joey.vanlierop@me.com>
2020-06-12 18:59:23 -0600

    Add name to global style component

--- src/components/GlobalStyle.jsx
+++ src/components/GlobalStyle.jsx
@@ -1,30 +1,35 @@
-import { Global, css } from "@emotion/core";
+import { css, Global } from "@emotion/core";
+import React from "react";
 
-export default () => (
-  <Global
-    styles={css`
-      html {
-        line-height: 1.5;
-        scroll-behavior: smooth;
-        box-sizing: border-box;
-      }
+function GlobalStyle() {
+  return (
+    <Global
+      styles={css`
+        html {
+          line-height: 1.5;
+          scroll-behavior: smooth;
+          box-sizing: border-box;
+        }
 
-      html,
-      body {
-        height: 100%;
-      }
+        html,
+        body {
+          height: 100%;
+        }
 
-      #root {
-        display: flex;
-        flex-direction: column;
-        min-height: 100%;
-      }
+        #root {
+          display: flex;
+          flex-direction: column;
+          min-height: 100%;
+        }
 
-      *,
-      *:before,
-      *:after {
-        box-sizing: inherit;
-      }
-    `}
-  />
-);
+        *,
+        *:before,
+        *:after {
+          box-sizing: inherit;
+        }
+      `}
+    />
+  );
+}
+
+export default GlobalStyle;
