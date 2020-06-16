1824f8df4ad72cd48b2719d5c0e4e4d0d1bd478b
Joey Van Lierop <joey.vanlierop@me.com>
2020-06-15 23:41:28 -0600

    Remove styled components from header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,22 @@
 import React from "react";
-import styled from "@emotion/styled";
-
-const StyledHeader = styled.section`
-  display: flex;
-  align-items: center;
-  flex-direction: row;
-  font-size: 3rem;
-  font-weight: 700;
-  padding-right: 1rem;
-  font-family: ${({ theme }) => theme.fonts.header};
-  color: ${({ theme }) => theme.colors.text};
-`;
 
 export function Header({ children }) {
-  return <StyledHeader>{children}</StyledHeader>;
+  return (
+    <section
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "row",
+        fontSize: "3rem",
+        fontWeight: "700",
+        paddingRight: "1rem",
+        fontFamily: "header",
+        color: "text",
+      }}
+    >
+      {children}
+    </section>
+  );
 }
 
 export default Header;
