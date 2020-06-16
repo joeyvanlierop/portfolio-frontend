457717bd92bc341343db071b7ade4028d2c9a592
Joey Van Lierop <joey.vanlierop@me.com>
2020-06-15 23:41:51 -0600

    Remove styled components from section

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,16 +1,20 @@
 import React from "react";
-import styled from "@emotion/styled";
-
-const StyledSection = styled.section`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  padding: 12rem 0;
-`;
 
 export function Section({ children, ...props }) {
-  return <StyledSection {...props}>{children}</StyledSection>;
+  return (
+    <section
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        padding: "12rem 0",
+      }}
+      {...props}
+    >
+      {children}
+    </section>
+  );
 }
 
 export default Section;
