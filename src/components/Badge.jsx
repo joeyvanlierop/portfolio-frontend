8e90930c645b992989659406d70ae533788a6556
Joey Van Lierop <joey.vanlierop@me.com>
2020-06-15 23:40:41 -0600

    Remove styled components from badge

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,19 +1,23 @@
 import React from "react";
-import styled from "@emotion/styled";
-
-const StyledBadge = styled.span`
-  color: ${({ theme }) => theme.colors.badge};
-  background-color: ${({ theme }) => theme.colors.gray};
-  font-family: ${({ theme }) => theme.fonts.badge};
-  font-size: 75%;
-  font-weight: 700;
-  line-height: 1;
-  padding: 0.25em 0.4em;
-  border-radius: 0.25rem;
-`;
 
 export function Badge({ children, ...props }) {
-  return <StyledBadge {...props}>{children}</StyledBadge>;
+  return (
+    <span
+      sx={{
+        color: "badge",
+        backgroundColor: "gray",
+        fontFamily: "badge",
+        fontSize: "75%",
+        fontWeight: "700",
+        lineHeight: "1",
+        padding: "0.25em 0.4em",
+        borderRadius: "0.25rem",
+      }}
+      {...props}
+    >
+      {children}
+    </span>
+  );
 }
 
 export default Badge;
