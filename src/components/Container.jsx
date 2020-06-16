19e804b8e7b36a3a311b93c8069df86ad9836c4d
Joey Van Lierop <joey.vanlierop@me.com>
2020-06-15 23:41:10 -0600

    Remove styled components from container

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,24 +1,25 @@
 import React from "react";
-import styled from "@emotion/styled";
-
-const ContainerWrapper = styled.div`
-  width: 100%;
-  display: flex;
-  align-content: center;
-  justify-content: center;
-`;
-
-const StyledContainer = styled.div`
-  padding: 0 2rem;
-  width: 100%;
-  max-width: 80rem;
-`;
 
 export function Container({ children }) {
   return (
-    <ContainerWrapper>
-      <StyledContainer>{children}</StyledContainer>
-    </ContainerWrapper>
+    <div
+      sx={{
+        width: "100%",
+        display: "flex",
+        alignContent: "center",
+        justifyContent: "center",
+      }}
+    >
+      <div
+        sx={{
+          padding: "0 2rem",
+          width: "100%",
+          maxWidth: "80rem",
+        }}
+      >
+        {children}
+      </div>
+    </div>
   );
 }
 
