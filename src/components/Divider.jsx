5f04a784e16712f0608e05069f7df4f264ec01da
Joey Van Lierop <joey.vanlierop@me.com>
2020-06-15 23:41:16 -0600

    Remove styled components from divider

--- src/components/Divider.jsx
+++ src/components/Divider.jsx
@@ -1,28 +1,28 @@
 import React from "react";
-import styled from "@emotion/styled";
-
-const DividerWrapper = styled.div`
-  margin: 0;
-  padding: 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-`;
-
-const StyledDivider = styled.hr`
-  margin: 0;
-  padding: 0;
-  width: 20%;
-  border: none;
-  border-bottom: 2px dotted ${({ theme }) => theme.colors.text};
-`;
 
 export function Divider() {
   return (
-    <DividerWrapper>
-      <StyledDivider />
-    </DividerWrapper>
+    <div
+      sx={{
+        margin: "0",
+        padding: "0",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100%",
+      }}
+    >
+      <hr
+        sx={{
+          margin: "0",
+          padding: "0",
+          width: "20%",
+          border: "none",
+          borderBottom: "2px dotted",
+          borderColor: "text",
+        }}
+      />
+    </div>
   );
 }
 
