da37a2f8decb4c5f3a8faceddd205b7a6cda2219
Joey Van Lierop <joey.vanlierop@me.com>
2020-06-15 23:42:14 -0600

    Remove styled components from splash

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -1,16 +1,19 @@
 import React from "react";
-import styled from "@emotion/styled";
-
-const StyledSplash = styled.section`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  height: 100vh;
-`;
 
 export function Splash({ children }) {
-  return <StyledSplash>{children}</StyledSplash>;
+  return (
+    <section
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        height: "100vh",
+      }}
+    >
+      {children}
+    </section>
+  );
 }
 
 export default Splash;
